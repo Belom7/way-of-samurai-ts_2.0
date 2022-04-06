241a2ffdb646f04c80e6a11da5aee90bc86fe9ec
Belom7 <Still07pkm@gmail.com>
2022-04-06 20:14:29 +0300

    18. Уроки React JS (страница Dialogs) - react курсы бесплатно

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/01_Header/Header";
 import {Navbar} from "./components/02_Navbar/Navbar";
-import {Profile} from "./components/03_Content/Profile";
+import {Profile} from "./components/03_Content/01_Profile/Profile";
+import {Dialogs} from "./components/03_Content/02_Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="App-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='App-wrapper-content'>
+                {/*<Profile/>*/}
+                <Dialogs/>
+            </div>
+
         </div>
     );
 }
