19528b9318ee9aa83e3c3f38bf3f23b3d444f404
Belom7 <Still07pkm@gmail.com>
2022-04-06 21:27:17 +0300

    20. Уроки React JS (NavLink) - react курсы бесплатно

--- src/components/02_Navbar/Navbar.tsx
+++ src/components/02_Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import cl from './Navbar.module.css'
 
 export const Navbar = () => {
     return (
         <div className={cl.navbar}>
-            <div><a href={'/profile'}>Profile</a></div>
-            <div><a href={'/messages'}>Messages</a></div>
-            <div><a href={'/news'}>News</a></div>
-            <div><a href={'/music'}>Music</a></div>
+            <div><NavLink to={'/profile'} className={({isActive})=>isActive? cl.active: ''}>Profile</NavLink></div>
+            <div><NavLink to={'/messages'} className={({isActive})=>isActive? cl.active: ''}>Messages</NavLink></div>
+            <div><NavLink to={'/news'} className={({isActive})=>isActive? cl.active: ''}>News</NavLink></div>
+            <div><NavLink to={'/music'} className={({isActive})=>isActive? cl.active: ''}>Music</NavLink></div>
         </div>
     );
 };
