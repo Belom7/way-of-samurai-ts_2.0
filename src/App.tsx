3c502f5862f6294064df67150bb6cdc99b5c5b95
Belom7 <Still07pkm@gmail.com>
2022-04-09 17:50:01 +0300

    28. Уроки React JS (выносим данные в index.js) - react курсы бесплатно, 2019

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,23 @@ import {Route, Routes} from "react-router-dom";
 import {Music} from "./components/03_Content/04_Music/Music";
 import {News} from "./components/03_Content/03_News/News";
 
-const posts = [
-    {name: 'Maks', message: 'Hi people!', likeCount: 200},
-    {name: 'Denis', message: 'x', likeCount: 20},
-    {name: 'Mark', message: 'wow!', likeCount: 10},
-    {name: 'Alex', message: 'perfecto!', likeCount: 50},
-    {name: 'Gera', message: 'rrrr!', likeCount: 50},
-]
-const dialogsName = [
-    {id: 1, name: 'Maks'},
-    {id: 2, name: 'Alex'},
-    {id: 3, name: 'Mikki'},
-    {id: 4, name: 'Jax'},
-    {id: 5, name: 'Xioru'},
-]
-const messages = [
-    {message: 'Здарова мужик!'},
-    {message: 'Как дела?'},
-    {message: 'Хо-хо-хо!'},
-    {message: 'Ничего страшного!'},
-    {message: '.........'}
-]
 
-function App() {
+type AppPropsType = {
+    posts: { name: string, message: string, likeCount: number }[]
+    dialogsName: { id: number, name: string }[]
+    messages: { message: string }[]
+}
+
+function App(props: AppPropsType) {
     return (
         <div className="App-wrapper">
             <Header/>
             <Navbar/>
             <div className='App-wrapper-content'>
                 <Routes>
-                    <Route path={'/profile'} element={<Profile posts={posts}/>}/>
-                    <Route path={'/messages'} element={<Dialogs dialogsName={dialogsName} messages={messages}/>}/>
+                    <Route path={'/profile'} element={<Profile posts={props.posts}/>}/>
+                    <Route path={'/messages'}
+                           element={<Dialogs dialogsName={props.dialogsName} messages={props.messages}/>}/>
                     <Route path={'/news'} element={<News/>}/>
                     <Route path={'/music'} element={<Music/>}/>
                 </Routes>
